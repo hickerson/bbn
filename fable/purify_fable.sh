ffa2f2f38d3a224655f2b173fba6330de75b5493
Kevin Peter Hickerson <hickerson@gmail.com>
2014-04-06 22:31:59 -0700

    Now can run a failed compile with 'make bbn'

--- fable/purify_fable.sh
+++ fable/purify_fable.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-sed -i 's/\/\/C/\/\//g' bbn.cpp
-sed -i 's/fem::float0/0/g' bbn.cpp
-sed -i 's/fem::int0/0/g' bbn.cpp
+for file in bbn.cpp cmn.hpp
+do
+	sed -i 's/\/\/C/\/\//g' $file
+	sed -i 's/fem::float0/0/g' $file
+	sed -i 's/fem::int0/0/g' $file
+done
