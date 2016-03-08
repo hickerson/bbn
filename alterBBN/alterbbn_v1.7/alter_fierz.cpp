c105eef4d21ed3718176c255cc849571f09810e0
Kevin Peter Hickerson <hickerson@gmail.com>
2016-03-08 11:39:26 -0800

    Cleaned up. Ready for next version, v1.7.1

--- alterBBN/alterbbn_v1.7/alter_fierz.cpp
+++ alterBBN/alterbbn_v1.7/alter_fierz.cpp
@@ -38,34 +38,21 @@ int main(int argc,char** argv)
 	
 	NuclideMap ratioH, sigma_ratioH;
 	NuclideIndex ratioIndex[6] = {He4,H2,He3,Li7,Li6,Be7};
-	//double H2_H,He3_H,Yp,Li7_H,Li6_H,Be7_H;
-	//double sigma_H2_H,sigma_He3_H,sigma_Yp,sigma_Li7_H,sigma_Li6_H,sigma_Be7_H;
 
 	printf("\t Yp\t\t H2/H\t\t He3/H\t\t Li7/H\t\t Li6/H\t\t Be7/H\n");
 	nucl(2,relic,ratioH);
-//	H2_H=ratioH[H2];Yp=ratioH[He4];Li7_H=ratioH[Li7];Be7_H=ratioH[Be7];He3_H=ratioH[He3];Li6_H=ratioH[Li6];
-//	printf("  low:\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\n",Yp,H2_H,He3_H,Li7_H,Li6_H,Be7_H);
 	printratios("  low:", ratioIndex, ratioH);
 
 	nucl(0,relic,ratioH);
-//	H2_H=ratioH[H2];Yp=ratioH[He4];Li7_H=ratioH[Li7];Be7_H=ratioH[Be7];He3_H=ratioH[He3];Li6_H=ratioH[Li6];
-	//printf(" cent:\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\n",Yp,H2_H,He3_H,Li7_H,Li6_H,Be7_H); 
 	printratios(" cent:", ratioIndex, ratioH);
 	
 	nucl(1,relic,ratioH);
-	//H2_H=ratioH[H2];Yp=ratioH[He4];Li7_H=ratioH[Li7];Be7_H=ratioH[Be7];He3_H=ratioH[He3];Li6_H=ratioH[Li6];
-	//printf(" high:\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\n\n",Yp,H2_H,He3_H,Li7_H,Li6_H,Be7_H);
 	printratios(" high:", ratioIndex, ratioH);
 			
 	if(nucl_witherrors(3,relic,ratioH,sigma_ratioH))
 	{
 		printf("With uncertainties:\n");
-	    //H2_H=ratioH[H2]; Yp=ratioH[He4]; Li7_H=ratioH[Li7]; Be7_H=ratioH[Be7]; He3_H=ratioH[He3]; Li6_H=ratioH[Li6];
-	    //sigma_H2_H=sigma_ratioH[H2];sigma_Yp=sigma_ratioH[He4];sigma_Li7_H=sigma_ratioH[Li7];sigma_Be7_H=sigma_ratioH[Be7];sigma_He3_H=sigma_ratioH[He3];sigma_Li6_H=sigma_ratioH[Li6];
 		printf("\t Yp\t\t H2/H\t\t He3/H\t\t Li7/H\t\t Li6/H\t\t Be7/H\n");
-		
-	    //printf("value:\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\n",Yp,H2_H,He3_H,Li7_H,Li6_H,Be7_H); 
-		//printf(" +/- :\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\t %.3e\n\n",sigma_Yp,sigma_H2_H,sigma_He3_H,sigma_Li7_H,sigma_Li6_H,sigma_Be7_H);
 	    printratios("value:", ratioIndex, ratioH);
 	    printratios("  +/-:", ratioIndex, sigma_ratioH);
 	}
