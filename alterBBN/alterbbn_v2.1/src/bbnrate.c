1d4775e0cd25254caee30b0685aa6acdc7ca903c
Kevin Peter Hickerson <hickerson@gmail.com>
2018-08-06 11:51:07 -0700

    succsefully unwraped fermi function parameter eta

--- alterBBN/alterbbn_v2.1/src/bbnrate.c
+++ alterBBN/alterbbn_v2.1/src/bbnrate.c
@@ -96,7 +96,7 @@ double rate_pn_enu(int type, double T9, double Tnu, relicparam* paramrelic, erro
 	norm = (double)n/(zmax-1.);
 
 	if(type== 1 || type == 4)
-		zmax = max(n*z9,znu*(n+xi)+0.999999*q);
+		zmax = max(n*z9,znu*(n+xi)+0.999999*q);		/// TODO why does it need 0.9999999? Fails if exactly q
 	else
 		zmax = max(n*z9,znu*(n-xi)-0.999999*q);
 
@@ -105,15 +105,10 @@ double rate_pn_enu(int type, double T9, double Tnu, relicparam* paramrelic, erro
 	//double zmax = q;
 	for(i=1;i<=n;i++)
 	{
-		//zmax = max(n*z9,znu*(n+xi)+q);
-		//norm = (double)n/(zmax-1.);
-		x = 1.+i/norm;
+		x = 1.+i/norm;				/// electron relativistic energy
+		beta = sqrt(x*x-1.);		/// TODO needs factor of m_e, check
+		eta = 2*pi*alpha/beta;		/// p and e are on the same side of the reverse reaction
 		//printf("x=%f type=%d zmax=%f q=%f\n", x, type, zmax, q);
-
-		//if(x>1.)
-		//{
-			beta = sqrt(x*x-1.);				/// TODO needs factor of m_e, check
-			eta = 2*pi*alpha/beta;			/// p and e are on the same side of the reverse reaction
 #if USE_BETA_SWITCH
 			if (type == 1 || type ==3)  {
 				eta = 2*pi*alpha/beta;			/// p and e are on the same side of the forward reaction
@@ -131,31 +126,28 @@ double rate_pn_enu(int type, double T9, double Tnu, relicparam* paramrelic, erro
 				/(1.+exp((x-q)/znu-xi));
 #else
 			switch(type) {
-				case 1:
-					eta = 2*pi*alpha/beta;			/// p and e are on the same side of the forward reaction
-					F = eta/(1-exp(-eta));
+				case 1:								/// n --> peu
+					F = eta/(1-exp(-eta)); 			/// p and e are on the same side of the forward reaction
 					dI = F*(x+b)*pow(x-q,2.)*beta   /// x+b, x-q, -x/z9, +(x-q)/znu, -xi -> +--+-
 						  /(1.+exp(-x/z9)) 					
 						  /(1.+exp((x-q)/znu-xi));
 					break;
 
-				case 2: 							/// p and e+ are on opposite sides of the same reaction
-					dI = (x-b)*pow(x+q,2.)*beta		/// x-b, x+q, +x/z9, -(x+q)/znu, -xi -> -++--
-						  /(1+exp(x/z9))
+				case 2: 							/// ne <--> pu
+					dI = (x-b)*pow(x+q,2.)*beta		/// p and e+ are on opposite sides of the same reaction
+						  /(1+exp(x/z9)) 			/// x-b, x+q, +x/z9, -(x+q)/znu, -xi -> -++--
 						  /(1+exp(-(x+q)/znu-xi));
 					break;
 
-				case 3:
-					eta = 2*pi*alpha/beta;			/// p and e are on the same side of the forward reaction
-					F = eta/(1-exp(-eta));
+				case 3:								/// nu --> pe
+					F = eta/(1-exp(-eta)); 			/// p and e are on the same side of the forward reaction
 					dI = F*(x-b)*pow(x+q,2.)*beta	/// x-b, x+q, -x/z9, +(x+q)/znu, +xi -> -+-++ 
 						  /(1+exp(-x/z9))
 						  /(1+exp((x+q)/znu+xi));
 					break;
 
-				case 4:
-					eta = -2*pi*alpha/beta;			/// p and e are on the same side of the reverse reaction
-					F = eta/(1-exp(-eta));
+				case 4:								/// pe --> nu
+					F = -eta/(1-exp(eta)); 			/// p and e are on the same side of the reverse reaction
 					dI = F*(x+b)*pow(x-q,2.)*beta	/// x+b, x-q, +x/z9, -(x-q)/znu, +xi -> +-+-+
 						  /(1+exp(x/z9))
 						  /(1+exp(-(x-q)/znu+xi)); 
